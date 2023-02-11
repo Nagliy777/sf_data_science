0510b7d610e8ccf7eb67193200e7d019dce28753
Nagliy777 <777egor77795@mail.ru>
2023-02-11 17:18:50 +0400

    test

--- IDE/project_final/game_v3.py
+++ IDE/project_final/game_v3.py
@@ -5,30 +5,46 @@
 import numpy as np
 
 
-def random_predict(number: int = 1) -> int:
-    """Рандомно угадываем число
-
+def game_core_v3(number: int = 1) -> int:
+    """
     Args:
         number (int, optional): Загаданное число. Defaults to 1.
 
     Returns:
         int: Число попыток
     """
+       
     count = 0
-
+    predict_number = np.random.randint(1, 101) # предполагаемое число
+    
     while True:
         count += 1
-        predict_number = np.random.randint(1, 101)  # предполагаемое число
-        if number == predict_number:
+        
+         
+        if number == predict_number:            
             break  # выход из цикла если угадали
+        elif number > predict_number:
+            predict_number += 1
+            if number == predict_number:
+                break  # выход из цикла если угадали
+            elif number > predict_number:
+                predict_number += 1
+        elif number < predict_number:
+          predict_number -= 1
+          if number == predict_number:
+              break  # выход из цикла если угадали
+          elif number < predict_number:
+                predict_number -= 1
+              
     return count
-
-
-def score_game(random_predict) -> int:
+    
+    
+    
+def score_game(game_core_v3) -> int:
     """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
 
     Args:
-        random_predict ([type]): функция угадывания
+        game_core_v3([type]): функция угадывания
 
     Returns:
         int: среднее количество попыток
@@ -38,7 +54,7 @@ def score_game(random_predict) -> int:
     random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
 
     for number in random_array:
-        count_ls.append(random_predict(number))
+        count_ls.append(game_core_v3(number))
 
     score = int(np.mean(count_ls))
    
@@ -48,4 +64,4 @@ def score_game(random_predict) -> int:
 
 if __name__ == "__main__":
     # RUN
-    score_game(random_predict)
+        score_game(game_core_v3)
